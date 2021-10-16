a5a5893cd4e6151d214970e260045ac67a2cfa42
NakraniKrupali0 <92012510+NakraniKrupali@users.noreply.github.com>
2021-10-16 14:00:38 +0000

    done

--- index.js
+++ index.js
@@ -1,97 +1,59 @@
-var rs = require("readline-sync")
-const chalk = require("chalk")
-
-let log = console.log
-
-let blueBright = chalk.bold.blueBright
-let green = chalk.bold.green
-let red = chalk.bold.red
-let cyan = chalk.bold.cyan
-let yellow = chalk.bold.yellow
-let title = chalk.black.bold.bgYellow
-let inverse = chalk.inverse
-
+var rs = require("readline-sync");
 var score = 0;
-var highscore = 15;
+var highscore = 30;
 
-let questions = [
-  {
-    no: '1',
-    question: `Who penned the lyrics to the songs from "Dilwale Dulhania Le Jayenge"?`,
-    A: 'R D Burman',
-    B: 'Anand Bakshi',
-    C: 'Cliff Richards',
-    answer: 'b',
-  },
-  {
-    no: '2',
-    question: `Who was the composer of "Kuch Kuch Hota Hai"?`,
-    A: 'Neil Sedaka',
-    B: 'O P Nayyar',
-    C: 'Jatin-Lalit',
-    answer: 'c',
-  },
-  {
-    no: '3',
-    question: `The first song, "Chali Aayee", is sung by whom?`,
-    A: 'Neil Sedaka',
-    B: 'Stevie Wonder',
-    C: 'Chitra and KK',
-    answer: 'c',
-  },
-  {
-    no: '4',
-    question: `Which of these is a song from the movie "Veer-Zaara"?`,
-    A: 'Main Yahaan Hoon',
-    B: 'Chup Chup Ke',
-    C: 'Silsila Ye Chahaat Ka',
-    answer: 'a',
-  },
-  {
-    no: '5',
-    question: `'Mughal-E-Azam' was one of the greatest films made in Bollywood, featuring many hit songs. Who was the music director of this film?`,
-    A: 'Elvis Presley',
-    B: 'Naushad',
-    C: 'Anu Malik',
-    answer: 'b',
-  },
-];
+const questions = require("./data");
 
-log(cyan("Welcome to the..."), title("Bollywood Trivia!\n"))
-let user = rs.question("Enter Your Name : ")
-log(green(`Hello ${user}`))
+const chalk = require("chalk");
 
-log(blueBright("\nA Quiz from our favourite music industry..."))
-log("\nThere will be 5 questions.")
-log(green("+5"), "points for correct answer.", red("-2"), "points for wrong answer.\n")
+let green = chalk.bold.green;
+let gray = chalk.bold.gray;
+let red = chalk.bold.red;
+let cyan = chalk.bold.cyan;
+let yellow = chalk.bold.yellow;
+let title = chalk.black.bold.bgYellow;
+let pink =chalk.bold.magenta;
+
+
+
+console.log(title("Welcome to Funny Quiz \n"));
+let user = rs.question("Enter Your Name : ");
+console.log(green(`Hello ${user}`));
+
+console.log(gray("\nHere's a bunch of funny tricky questions that will really get your brain. So, let's get your mind ready for the mess up!"));
+console.log("\nThere will be 5 questions.");
+console.log(green("+5"), "points for correct answer.", red("-2"), "points for wrong answer.\n");
 
 for (q of questions) {
-  log(yellow(q.no + ': ' + q.question));
-  log(cyan('A: ' + q.A));
-  log(cyan('B: ' + q.B));
-  log(cyan('C: ' + q.C));
+  console.log(yellow(q.no + ': ' + q.question));
+  console.log(cyan('A: ' + q.A));
+  console.log(cyan('B: ' + q.B));
+  console.log(cyan('C: ' + q.C));
+  console.log(cyan('D: ' + q.D));
+
 
   //input answer
   var ans = rs.question('Answer : ');
 
   //compare answer
-  if (ans.toLowerCase() === q.answer.toLowerCase()) {
-    log(green("\n+5"), "Your answer is", green("correct"), ".");
+  if (ans.toUpperCase() === q.answer) {
+    console.log(green("\n+5"), "Your answer is", green("correct"), ".");
     score += 5;
-    log("Your current score is", green(score), "\n");
+    console.log("Your current score is", green(score), "\n");
   } else {
-    log(red("+2"), "Your answer is", red("incorrect"), ".");
+    console.log(red("-2"), "Your answer is", red("incorrect"), ".");
+    console.log("Correct Answer :", green(q.answer));
     score -= 2;
-    log("Your current score is", red(score), "\n");
+    console.log("Your current score is", red(score), "\n");
   }
 }
 
-
+//check the performance
 if (score > highscore) {
-  log(green("Congratulation..!You Beat the best score."))
+  console.log(green("Congratulation..!You Beat the best score."))
 } else {
-  log(red("You couldn't beat the highscore. Better luck next time!\n"))
+  console.log(red("You couldn't beat the highscore . Better luck next time!\n"))
 }
 
-log(yellow("Scoreboard:"))
-log('Hello ' + red(user) + ' Your final score is : ' + score)
+console.log(yellow("Scoreboard:"))
+console.log('Hello ' + pink(user) + ' Your final score is : ' + score)
